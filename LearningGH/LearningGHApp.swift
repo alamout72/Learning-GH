28bdc96e69631fc2e40a55d3fdd215a277fbb4dc
Jay van Berkel <jay@ridict.com>
2021-06-29 15:34:27 +0200

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Jay van Berkel on 29.06.21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
